2eb972503da63234d5ef8d8af3c0b9674d50e01a
gracefulcanada@gmail.com <gracefulcanada@gmail.com>
2019-05-31 01:30:24 -0700

    start menu

--- app/src/main/java/com/example/pickrecipes/MainActivity.java
+++ app/src/main/java/com/example/pickrecipes/MainActivity.java
@@ -8,30 +8,12 @@ import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-{
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_k_list);
+        setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewList);
-
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(layoutManager);
-
-        List<CardList> dataList =  new ArrayList<>();
-        dataList.add(new CardList("[Rice]", "Bibimbap", R.drawable.kfood_thumb1));
-        dataList.add(new CardList("[Soup]", "Korean Kimchi", R.drawable.kfood_thumb2));
-        dataList.add(new CardList("[Soup]", "Korean Kimchi", R.drawable.kfood_thumb3));
-        dataList.add(new CardList("[Soup]", "Korean Kimchi", R.drawable.kfood_thumb1));
-        dataList.add(new CardList("[Soup]", "Korean Kimchi", R.drawable.kfood_thumb2));
-        dataList.add(new CardList("[Soup]", "Korean Kimchi", R.drawable.kfood_thumb3));
-        dataList.add(new CardList("[Soup]", "Korean Kimchi", R.drawable.kfood_thumb1));
-
-        RecyclerViewListAdapter adapter = new RecyclerViewListAdapter(dataList, this);
-        recyclerView.setAdapter(adapter);
     }
 }
