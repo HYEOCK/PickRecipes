d0faa7b3a71f23d48e585ef170301627c73557ad
gracefulcanada@gmail.com <gracefulcanada@gmail.com>
2019-05-31 09:49:07 -0700

    finished menu

--- app/src/main/java/com/example/pickrecipes/contents.java
+++ app/src/main/java/com/example/pickrecipes/contents.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,5 +29,38 @@ public class Contents extends AppCompatActivity {
 
         tvTitle.setText(Title);
         img.setImageResource(image);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            case R.id.action1:
+                Intent intent1 = new Intent(this, TfoodList.class);
+                startActivity(intent1);
+            case R.id.action2:
+                Intent intent2 = new Intent(this, KfoodList.class);
+                startActivity(intent2);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void taiwanFood(View view) {
+        Intent intent1 = new Intent(this, TfoodList.class);
+        startActivity(intent1);
+    }
+
+    public void koreanFood(View view) {
+        Intent intent2 = new Intent(this, KfoodList.class);
+        startActivity(intent2);
     }
 }
